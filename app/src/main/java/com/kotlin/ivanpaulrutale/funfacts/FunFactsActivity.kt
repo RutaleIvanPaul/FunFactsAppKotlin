0a415b738958391ee77472281999a2b0be81b601
RutaleIvanPaul <paulivan568@gmail.com>
2019-07-03 15:38:28 +0300

    Initiate Project

---
+++ app/src/main/java/com/kotlin/ivanpaulrutale/funfacts/FunFactsActivity.kt
@@ -0,0 +1,12 @@
+package com.kotlin.ivanpaulrutale.funfacts
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FunFactsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fun_facts)
+    }
+}
